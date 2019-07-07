0619c924df693c8b5a1d9354cf20c07e90ba44d7
Phil Mass <philrmass@gmail.com>
2019-07-07 09:06:56 -0700

    Updates

---
+++ precache-manifest.9cbc176e269b1e3916d0fe70c931aa48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "544512f440a241076b48",
+    "url": "/hunger-habit/static/js/runtime~main.6a89f152.js"
+  },
+  {
+    "revision": "68b7ce427a7ad4b3efc5",
+    "url": "/hunger-habit/static/js/main.08ddace3.chunk.js"
+  },
+  {
+    "revision": "10bf244ca692441702db",
+    "url": "/hunger-habit/static/js/2.1b3a0643.chunk.js"
+  },
+  {
+    "revision": "68b7ce427a7ad4b3efc5",
+    "url": "/hunger-habit/static/css/main.6581007a.chunk.css"
+  },
+  {
+    "revision": "d9ca0e116c26abf5cd58cf2bed2c7ddf",
+    "url": "/hunger-habit/index.html"
+  }
+];
